9e3ba71c98150b0f7d6bd799e5c8182de0e25a76
hailia sommerville <hailiassommerville@gmail.com>
2025-09-24 18:34:55 -0400

    card class implemented

---
+++ Card.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Card
+{
+    //Fields, example: Rank rank;
+    //check the help documentation for the fields
+    private Suit suit;
+    private Rank rank;
+    private bool isFaceUp;
+
+    //Card Constructor
+    public Card(Suit suit, Rank rank)
+    {
+        this.suit = suit;
+        this.rank = rank;
+        this.isFaceUp = false; 
+    }
+
+    //Define properties for all above fields
+    //code example: public Suit Suit { get { return suit; } }
+
+    public Suit Suit
+    {
+        get { return suit; }
+    }
+
+    public Rank Rank
+    {
+        get { return rank; }
+    }
+
+    public bool IsFaceUp
+    {
+        get { return isFaceUp; }
+    }
+
+
+    public void FlipOver()
+    {
+        //implementation 
+        isFaceUp = !isFaceUp;
+    }
+        
+}
